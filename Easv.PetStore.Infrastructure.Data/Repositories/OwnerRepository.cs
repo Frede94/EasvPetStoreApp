dce461be297bffd0b09023d68b91b6a32ca2dcb5
Frede94 <Frede94@users.noreply.github.com>
2018-09-18 13:41:43 +0200

    Semi implemented owner

--- Easv.PetStore.Infrastructure.Data/Repositories/OwnerRepository.cs
+++ Easv.PetStore.Infrastructure.Data/Repositories/OwnerRepository.cs
@@ -2,13 +2,38 @@
 using Easv.PetStore.Core.Entity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Easv.PetStore.Infrastructure.Data.Repositories
 {
     public class OwnerRepository : IOwnerRepository
     {
+        private PetStoreAppContext _PSActx;
+
+        public OwnerRepository(PetStoreAppContext PSActx)
+        {
+            _PSActx = PSActx;
+        }
+
         public Owner Create(Owner owner)
+        {
+            var o = _PSActx.Owners.Add(owner).Entity;
+            _PSActx.SaveChanges();
+            return o;
+        }
+
+        public IEnumerable<Owner> ReadAll()
+        {
+            return _PSActx.Owners;
+        }
+
+        public Owner ReadById(int id)
+        {
+            return _PSActx.Owners.FirstOrDefault(o => o.OwnerId == id);
+        }
+
+        public Owner Update(Owner ownerUpdate)
         {
             throw new NotImplementedException();
         }
@@ -18,19 +43,5 @@ namespace Easv.PetStore.Infrastructure.Data.Repositories
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Owner> ReadAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Owner ReadById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Owner Update(Owner ownerUpdate)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
